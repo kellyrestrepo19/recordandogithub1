8404ae318e5b88fb9d9bb5eb76040d43a0c56a18
Your Name <you@example.com>
2023-08-15 08:37:16 -0500

    feat:Se resciben datos por teclado y se castea

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,4 +1,4 @@
-nombreUsuario = "Kelly Yiceli Muñoz Restrepo"
+'''nombreUsuario = "Kelly Yiceli Muñoz Restrepo"
 edadUsuario = 19
 estaturaUsuario = 1.59
 esHinchaDelVerde = True
@@ -8,3 +8,14 @@ print(f"Su nombre es:{nombreUsuario} y tienes {edadUsuario} años tu estatura es
 comidasFavoritas = ["frijoles0","sushicharron", "carne molida","mazamorra"]
 print(comidasFavoritas)
 print(comidasFavoritas[2])
+
+#Entradas por teclado:
+lugarTrabajoUsuario = input("Digita el lugar de tu trabajo: ")
+print(f"Usted trabaja en: {lugarTrabajoUsuario}")'''
+
+#Pedimos dos numeros 
+
+numerouno = int(input("Digita un numero:"))
+numeroDos = int(input("Digita otro numero:"))
+sumatoria = numerouno+numeroDos
+print(f"la suma de {numerouno}+{numeroDos}={sumatoria}")
