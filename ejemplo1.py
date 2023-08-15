8ccd613e1eda9fafa7ad148cc80269c94d369a2c
Your Name <you@example.com>
2023-08-15 08:06:12 -0500

    feat: se crean variables primitivas y lista de datos

---
+++ ejemplo1.py
@@ -0,0 +1,10 @@
+nombreUsuario = "Kelly Yiceli Muñoz Restrepo"
+edadUsuario = 19
+estaturaUsuario = 1.59
+esHinchaDelVerde = True
+
+print(f"Su nombre es:{nombreUsuario} y tienes {edadUsuario} años tu estatura es:{estaturaUsuario} ")
+
+comidasFavoritas = ["frijoles0","sushicharron", "carne molida","mazamorra"]
+print(comidasFavoritas)
+print(comidasFavoritas[2])
